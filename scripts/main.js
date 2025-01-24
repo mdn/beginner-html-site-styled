b7e3674e7231f6b4486348ef9af07f89ab3369a5
Gabriel-Ramirez-techlab <gabriel.ramirez24@Student.CART.org>
2025-01-24 02:22:11 -0800

    k

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,40 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/j.jpg");
+    myImage.setAttribute("src", "images/cool-3.png");
   } else {
-    myImage.setAttribute("src", "images/");
+    myImage.setAttribute("src", "images/firefox-icon.png");
   }
 });
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("hi");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
